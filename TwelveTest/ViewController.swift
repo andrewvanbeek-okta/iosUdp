e649770c1a4791d7889483ea2c418052f8757712
Andrew Van Beek <andrew.vanbeek@okta.com>
2019-05-10 11:01:54 -0400

    Initial Commit

---
+++ TwelveTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwelveTest
+//
+//  Created by Andrew Vanbeek on 5/10/19.
+//  Copyright © 2019 Andrew Vanbeek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
